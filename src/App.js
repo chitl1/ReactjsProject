21bf690f2872b2be797dc4627b3524370a4a9ee3
Chi <thanlinhchi010995.dhcn@gmail.com>
2023-11-04 13:35:09 +0700

    random gift

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+const gifts = [
+  'keo',
+  'banh',
+  'nha',
+  'oto',
+  '1ty'
+]
 function App() {
+  const [gift, setgift] = useState();
+  const randomGift = () =>  {
+    const i = Math.floor(Math.random() * gifts.length)
+    setgift(gifts[i])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> { gift || "chua co phan thuwong"}</h1>
+      <button onClick={randomGift}>
+        lay thuong
+      </button>
     </div>
   );
 }
